f3589318ae48fcc4395e3abcf192da1dd8de6b49
Vitor Rolisola <66443241+vitorrolisola@users.noreply.github.com>
2020-06-07 18:35:46 -0300

    primeiro exercicio

---
+++ Exercícios para entrega/pesoIdeal/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Faça um programa que receba a altura (em metro) e o sexo (M ou F) de uma pessoa e que calcule e mostre o seu peso ideal, utilizando as seguintes fórmulas:
+
+    Masculino (72.7*h)-58
+    Feminino  (62.1*h)-44.7
+
+*/
+
+void main()
+{
+    float h;
+    char sexo;
+
+    printf("Digite o sexo da pessoa: F ou M\n");
+    scanf(" %c", &sexo);
+}
