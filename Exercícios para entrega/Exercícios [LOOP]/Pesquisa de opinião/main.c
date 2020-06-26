7347bab081ea0ffdde924ca8ab3b0949a8c22213
Vitor Rolisola <66443241+vitorrolisola@users.noreply.github.com>
2020-06-26 18:41:17 -0300

    Reorganizando pastas

---
+++ Exercícios para entrega/Exercícios [LOOP]/Pesquisa de opinião/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Cada espectador de um cinema respondeu a um questionário no qual constava sua idade e a sua opinião (Tabela abaixo) em relação ao filme assistido. Faça um programa que receba a idade e a opinião de 15 espectadores e que calcule e mostre:
+
+        a média das idades das pessoas que responderam ótimo;
+        a quantidade de pessoas que responderam regular;
+        a percentagem de pessoas que responderam bom entre todos os espectadores analisados.
+
+        Opinião	    Símbolo
+        Regular	       1
+        Bom	           2
+        Ótimo	       3
+
+    Observações:
+        Os resultados deverão ser impressos na ordem que foram listados acima, separados por espaço.
+        Insira o sinal de porcentagem após a variável referente a este cálculo.
+
+    Exemplo:
+        Entrada:
+            67 3 71 3 74 3 31 3 ...
+        Saída:
+            59.200001 7 20.000000%
+*/
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
